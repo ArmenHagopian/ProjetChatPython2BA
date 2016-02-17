adc9d3ae6fee3bdd31d2601cc29d945597339639
Gaétan Guru <gaetan.guru@gmail.com>
2016-02-17 14:39:30 +0100

    Initial Commit

---
+++ chat2.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  chat2.py
+#  
+#  Copyright 2016 Gaétan Guru <gaetanguru@MacBook-Pro-de-Gaetan-3.local>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+#!/usr/bin/env python3
+# chat.py
+# author: Sébastien Combéfis
+# version: February 15, 2016
+
+import socket
+import sys
+import threading
+
+class Chat():
+    def __init__(self, host=socket.gethostname(), port=5100):
+        s = socket.socket(type=socket.SOCK_DGRAM)
+        s.settimeout(0.5)
+        s.bind((host, port))
+        self.__s = s
+        print('Écoute sur {}:{}'.format(host, port))
+        
+    def run(self):
+        handlers = {
+            '/exit': self._exit,
+            '/quit': self._quit,
+            '/join': self._join,
+            '/send': self._send
+        }
+        self.__running = True
+        self.__address = None
+        threading.Thread(target=self._receive).start()
+        while self.__running:
+            line = sys.stdin.readline().rstrip() + ' '
+            # Extract the command and the param
+            command = line[:line.index(' ')]
+            param = line[line.index(' ')+1:].rstrip()
+            # Call the command handler
+            if command in handlers:
+                try:
+                    handlers[command]() if param == '' else handlers[command](param)
+                except:
+                    print("Erreur lors de l'exécution de la commande.")
+            else:
+                print('Command inconnue:', command)
+    
+    def _exit(self):
+        self.__running = False
+        self.__address = None
+        self.__s.close()
+    
+    def _quit(self):
+        self.__address = None
+    
+    def _join(self, param):
+        tokens = param.split(' ')
+        if len(tokens) == 2:
+            try:
+                self.__address = (tokens[0], int(tokens[1]))
+                print('Connecté à {}:{}'.format(*self.__address))
+            except OSError:
+                print("Erreur lors de l'envoi du message.")
+    
+    def _send(self, param):
+        if self.__address is not None:
+            try:
+                message = param.encode()
+                totalsent = 0
+                while totalsent < len(message):
+                    sent = self.__s.sendto(message[totalsent:], self.__address)
+                    totalsent += sent
+            except OSError:
+                print('Erreur lors de la réception du message.')
+    
+    def _receive(self):
+        while self.__running:
+            try:
+                data, address = self.__s.recvfrom(1024)
+                print(data.decode())
+            except socket.timeout:
+                pass
+            except OSError:
+                return
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        Chat(sys.argv[1], int(sys.argv[2])).run()
+    else:
+        Chat().run()
