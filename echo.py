808f6cf40c8b13c1dfdf98d5978dc650b86d009a
ArmenHagopian <14040@ecam.be>
2016-02-25 22:18:21 +0100

    Other fusion tests

--- echo.py
+++ echo.py
@@ -1,6 +1,9 @@
 import socket
 import sys
 import threading
+import struct
+import pickle
+
 
 # faire en sort d'avoir le mm serveur pour EchoServer et EchoClient
 
@@ -17,18 +20,23 @@ class EchoServer():
         self.__port = port
 
     def run(self):
-        handlers = {
-            '/list': self._clientslist,
-        }
-        self.__s.listen(0)
+        self.__s.listen()
         while True:
             client, addr = self.__s.accept()
-            try:
-                clientslist[self.__host] = self.__port
-                print(self._receive(client).decode())
-                client.close()
-            except OSError:
-                print('Erreur lors de la réception du message.')
+            print(addr)
+            print(self._receive(client).decode())
+            print(self._send())
+
+
+    def _send(self):
+        self.__address = (self.__host, 7000)
+        if self.__address is not None:
+                message = 'hello'.encode()
+                totalsent = 0
+                while totalsent < len(message):
+                    sent = self.__s.sendto(message[totalsent:], self.__address)
+                    totalsent += sent
+
 
     def _receive(self, client):
         chunks = []
@@ -46,10 +54,10 @@ class EchoServer():
 
 
 class EchoClient():
-    def __init__(self, host=socket.gethostname(), port=6000):
-        s = socket.socket(type=socket.SOCK_DGRAM)
+    def __init__(self, host=socket.gethostname(), port=6000, message=None):
+        self.__message = message
+        s = socket.socket()
         s.settimeout(0.5)
-        s.bind((host, port))
         self.__s = s
         print('Écoute sur {}:{}'.format(host, port))
         self.__host = host
@@ -57,12 +65,15 @@ class EchoClient():
 
     def run(self):
         handlers = {
-            '/list': self._clientslist,
             '/exit': self._exit,
             '/quit': self._quit,
             '/join': self._join,
-            '/send': self._send
+            '/send': self._send_peer
         }
+        if self.__message:
+            self.__s.connect((self.__host, self.__port))
+            self._send_server()
+            self.__s.close()
         self.__running = True
         self.__address = None
         threading.Thread(target=self._receive).start()
@@ -72,21 +83,21 @@ class EchoClient():
             # Extract the command and the param
             command = line[:line.index(' ')]
             param = line[line.index(' ')+1:].rstrip()
+
             # Call the command handler
             if command in handlers:
-                try:
                     handlers[command]() if param == '' else handlers[command](param)
-                except:
-                    print("Erreur lors de l'exécution de la commande.")
+
             else:
                 print('Command inconnue:', command)
+        self.__s.close()
 
-        try:
-            self.__s.connect((self.__host, self.__port))
-            self._send(param)
-            self.__s.close()
-        except OSError:
-            print('Serveur introuvable, connexion impossible.')
+        # try:
+        #     self.__s.connect((self.__host, self.__port))
+        #     self._send(param)
+        #     self.__s.close()
+        # except OSError:
+        #     print('Serveur introuvable, connexion impossible.')
 
     def _exit(self):
         self.__running = False
@@ -101,11 +112,11 @@ class EchoClient():
         if len(tokens) == 2:
             try:
                 self.__address = (tokens[0], int(tokens[1]))
-                print('Connecté à {}:{}'.format(*self.__address))
+                print('Connecté en tant que {}:{}'.format(*self.__address))
             except OSError:
                 print("Erreur lors de l'envoi du message.")
 
-    def _send(self, param):
+    def _send_peer(self, param):
         if self.__address is not None:
             try:
                 message = param.encode()
@@ -115,6 +126,17 @@ class EchoClient():
                     totalsent += sent
             except OSError:
                 print('Erreur lors de la réception du message.')
+
+    def _send_server(self):
+        totalsent = 0
+        msg = self.__message
+        try:
+            while totalsent < len(msg):
+                sent = self.__s.send(msg[totalsent:])
+                totalsent += sent
+        except OSError:
+            print("Erreur lors de l'envoi du message.")
+
 
     def _receive(self):
         while self.__running:
@@ -126,17 +148,13 @@ class EchoClient():
             except OSError:
                 return
 
-    def _clientslist(self):
-        for client in clientslist['connectedclients']:
-            print(client)
-        return ''
 
 if __name__ == '__main__':
     if len(sys.argv) == 4 and sys.argv[1] == 'server':
         EchoServer(sys.argv[2], int(sys.argv[3])).run()
         # SERVERADDRESS = (socket.gethostname(), 6000)
 
+    # elif len(sys.argv) == 4 and sys.argv[1] == 'client':
+    #     EchoClient(sys.argv[2], int(sys.argv[3])).run()
     elif len(sys.argv) == 5 and sys.argv[1] == 'client':
-        EchoClient(sys.argv[2], int(sys.argv[3])).run()
-    elif len(sys.argv) == 2 and sys.argv[1] == 'client':
-        EchoClient().run()
+        EchoClient(sys.argv[2], int(sys.argv[3]), sys.argv[4].encode()).run()
